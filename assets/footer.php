5f9fe2a0572dd7b062334e85a106b292f046ee0c
WillSylvan <silvis.silvestrs@gmail.com>
2017-12-01 16:34:42 +0200

    -a

---
+++ assets/footer.php
@@ -0,0 +1,67 @@
+<link rel="stylesheet" type="text/css" href="style/footer.css">
+
+
+<div class="footer">
+		
+    <h1 class="phone">+371 22334455</h1>
+    
+    <div class="site-icons"></div>
+
+    <div class="inline">
+        <div class="inline">
+
+            <div class="block">
+
+                <h3>kontakti</h3>
+
+                <p>čats ar konultantu</p>
+                <p>info@atrum.lv</p>
+                <p>latcredit.lv</p>
+            </div>
+
+            <div class="block">
+
+                <h3>rekvezīti</h3>
+
+                <p>LATEKO LĪZINGS AS </p>
+                <p>Reģ.Nr40003366651 </p>
+                <p>Katlakalna iela 1, </p>
+                <p>Rīga, LV-1073 </p>
+            </div>
+
+            <div class="block">
+
+                <h3>svarīgi zināt</h3>
+
+                <ul>
+                    <li><a href="default.asp">Jautājumi un Atbildes</a></li>
+                    <li><a href="news.asp">Kā atmaksāt?</a></li>
+                    <li><a href="contact.asp">Kā saņemt?</a></li>
+                    <li><a href="about.asp">Kontakti</a></li>
+                    <li><a href="about.asp">Līgums</a></li>
+                </ul>
+            </div>
+
+            <div class="block">
+
+                <h3>Juradiskā adrese</h3>
+
+                <p>Privātuma atruna</p>
+                <p>Autortiesības</p>
+        
+            </div>
+        </div>
+
+        <div class="block2">
+
+            <div class="sites">
+                
+                <h3>Darba laiks</h3>
+
+                <p>Darba dienās: 8:00 - 22:00</p>
+                <p>Brīvdienas: 8:00 - 22:00</p>
+            </div>
+
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>     
+		<!--<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Baloo" rel="stylesheet">-->
+		<link rel="stylesheet" type="text/css" href="style/index.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
+        <title></title>
+		
+		<script type="text/javascript" src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+    </head>
+
+	<body>
+	
+		<?php include 'assets/header.php'; ?>
+	
+
+
+
+
+
+
+
+		<?php include 'assets/footer.php'; ?>
+		
+	</body>
+</html>
