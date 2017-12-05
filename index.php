498a851d1b04a2364db2f360d1d9b1264b02c8f5
WillSylvan <silvis.silvestrs@gmail.com>
2017-12-05 16:18:11 +0200

    atrum2

--- index.php
+++ index.php
@@ -347,7 +347,47 @@
 					evt.currentTarget.className += " active";
 				}
 			</script>
-			
+
+			<div class="headline">
+				<h1>Jautājumu un Atbildes</h1>
+			</div>
+
+			<button class="accordion">Section 1</button>
+
+			<div class="panel">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+
+			<button class="accordion">Section 2</button>
+
+			<div class="panel">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+
+			<button class="accordion">Section 3</button>
+			<div class="panel">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+
+
+
+			<script>
+				var acc = document.getElementsByClassName("accordion");
+				var i;
+
+				for (i = 0; i < acc.length; i++) {
+					acc[i].onclick = function() {
+						this.classList.toggle("active");
+						var panel = this.nextElementSibling;
+						if (panel.style.maxHeight){
+						panel.style.maxHeight = null;
+						} else {
+						panel.style.maxHeight = panel.scrollHeight + "px";
+						} 
+					}
+				}
+			</script>
+
 			<?php include 'assets/footer.php'; ?>
 		</div>
 
