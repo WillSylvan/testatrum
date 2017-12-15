52a38b8afb9c397bd743b1d0a4a9b7660c70c8ba
innadev1 <innad2201@gmail.com>
2017-12-15 16:36:51 +0200

    new

--- esosie_krediti.php
+++ esosie_krediti.php
@@ -22,7 +22,148 @@
 		</div>
 		<style>.bt4 button{background-color:rgb(255, 117, 130)}</style>
 	
+		<div id="aizdevumi">
 		
+			<div id="istermina">
+			
+				<h2>
+					<span>Īstermiņā</span> aizdevumi
+				</h2>
+				
+				<p>
+					Ja Jums ir radušies jautājumi par kredītiem vai atmaksu, lūdzam sūtīt ziņu<br>
+					uz <b> info@atrum.lv </b> minot kredīta Nr. un pieteikšanās datumu.
+				</p>
+				
+				<div class="table-aizdevumi">
+				
+					<div>
+						<div><h1>Kredīta nr.</h1></div>
+						<div><h1>Statuss</h1></div>
+						<div class="money"><h1>Kredīts</h1></div>
+						<div class="money"><h1>Komisija</h1></div>
+						<div><h1>Dienu skaits</h1></div>
+						<div><h1>Atmaksāt līdz</h1></div>
+						<div><h1>Kopā atmaksāt</h1></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="green">Aktīvs</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="yellow">Pieprasīts</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="violetc">Pagarināts</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="light-green">Apstiprināts</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="red">Kavēts</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+				</div>
+				
+				<div class="additional-info flex">
+					<div>
+						<p>13.03.2017 12.23<br>
+						<span>Eiropas pateriņa kredīta standarinformācija</span></p>
+					</div>
+					<div><button>Skatīt visus</button></div>
+				</div>
+				
+			</div>
+			
+			<div id="ilgtermina">
+				<h2>
+					<span>Ilgtermiņa</span> aizdevumi
+				</h2>
+				
+				<p>
+					Ja Jums ir radušies jautājumi par kredītiem vai atmaksu, lūdzam sūtīt ziņu<br>
+					uz <b> info@atrum.lv </b> minot kredīta Nr. un pieteikšanās datumu.
+				</p>
+				
+				<div class="table-aizdevumi">
+				
+					<div>
+						<div><h1>Kredīta nr.</h1></div>
+						<div><h1>Statuss</h1></div>
+						<div class="money"><h1>Kredīts</h1></div>
+						<div class="money"><h1>Komisija</h1></div>
+						<div><h1>Dienu skaits</h1></div>
+						<div><h1>Atmaksāt līdz</h1></div>
+						<div><h1>Kopā atmaksāt</h1></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="pinkc">Anulēts</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+					<div class="with-borders">
+						<div class="nr"><h3>5647-0</h3><p>20.12.2017</p></div>
+						<div class="nr"><h3><span class="greyc">Atmaksāts</span></h3><p>20.12.2017</p></div>
+						<div class="money"><p>$200</p></div>
+						<div class="money"><p>$20.00</p></div>
+						<div><p>30 dienas</p></div>
+						<div><p>20.12.2017</p></div>
+						<div><p><b>$220.00</b></p></div>
+					</div>
+					
+				</div>
+				
+				<div class="additional-info flex">
+					<div>
+						<p>13.03.2017 12.23<br>
+						<span>Eiropas pateriņa kredīta standarinformācija</span></p>
+					</div>
+					<div><button>Skatīt visus</button></div>
+				</div>
+				
+			</div>
+			
+		</div>
 		
 		<?php include 'assets/footer.php'; ?>	
 	</body>
