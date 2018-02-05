3ee36a9d4be83d9f399553775aed27c611b2d38e
Ceslavs Gutfrics <c.gutfrics@excordo.lv>
2018-02-05 11:41:38 +0200

    POST method touch server, form inputs edited, lang files added. Declared adress and IP need to be made.

--- js/registration.js
+++ js/registration.js
@@ -8,55 +8,124 @@ $(document).ready(function() {
   // Mask for inputs.
   //////////////////////////////////////////
 
-  $('input[name="p_code"]').mask('000000-00000');
+  $('input[name="identityCode"]').mask('000000-00000');
   $('input[name="phone"]').mask('00000000');
 
   //////////////////////////////////////////
-  // Translation. (Going to be as separate files lv and ru)
-  //////////////////////////////////////////
-
-  var mylanguage = {
-          errorTitle: 'Form submission failed!',
-          requiredFields: 'You have not answered all required fields',
-          badEmail: 'You have not given a correct e-mail address',
-          badTelephone: 'You have not given a correct phone number',
-          badSecurityAnswer: 'You have not given a correct answer to the security question',
-          lengthBadStart: 'The input value must be between ',
-          lengthBadEnd: ' characters',
-          lengthTooLongStart: 'The input value is longer than ',
-          lengthTooShortStart: 'The input value is shorter than ',
-          notConfirmed: 'Input values could not be confirmed',
-          badCustomVal: 'The input value is incorrect',
-          andSpaces: ' and spaces ',
-          badInt: 'The input value was not a correct number',
-          badStrength: 'The password isn\'t strong enough',
-          min : 'min',
-          max : 'max'
-      };
-  //////////////////////////////////////////
-  // Form navigation & validation.
+  // Client-side validation.
   //////////////////////////////////////////
   $.validate({
     modules : 'security, toggleDisabled',
   form : '#form-1 , #form-2 , #form-3',
-  lang: 'mylanguage'
+  lang: 'lv , ru'
 });
 
-$('input')
-  .on('beforeValidation', function(value, lang, config) {
-    console.log('Input "'+this.name+'" is about to become validated');
-    // Call $(this).attr('data-validation-skipped', 1); to prevent validation
-  });
-  var currentForm = '1';
+  //////////////////////////////////////////
+  // Form navigation & 'POST' to server api.
+  //////////////////////////////////////////
+
+  // Creating some variables, such as first form to show, array for fully filled form.
+  var currentForm = 3;
   $("#form-" + currentForm).show();
+  var allDataForm = [];
+  var customerData = {};
+
+  //////////////////////////////////////////
+  // Function to push current form data in to global form data array.
+  //////////////////////////////////////////
+
+  var serializeForm = function () {
+    if (currentForm === 1) {
+      var firstForm = $("#one-form").serializeArray();
+      allDataForm.push.apply(allDataForm , firstForm);
+    } else if ( currentForm === 2) {
+      var secondForm = $("#two-form").serializeArray();
+    //  var adressString = secondForm.join([seperator = ' ']);
+      allDataForm.push.apply(allDataForm , secondForm);
+    } else if ( currentForm === 3 ) {
+      var thirdForm = $("#three-form").serializeArray();
+      allDataForm.push.apply(allDataForm , thirdForm);
+    }
+  }
+
+  //////////////////////////////////////////
+  // Process all form data, to remove ("name" : name, "value" = value) format style.
+  //////////////////////////////////////////
+
+  var test = function() {
+    var processingArr = allDataForm
+   , processedData = {};
+
+ for (var i in processingArr) {
+   processedData[processingArr[i].name] = processingArr[i].value;
+ }
+
+ customerData["customerData"] = processedData;
+  }
+
+  //////////////////////////////////////////
+  // Ajax send function.
+  //////////////////////////////////////////
+
+  var sendToApi = function () {
+    console.log("sending to api");
+  var createCORSRequest = function(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+      // Most browsers.
+      xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+      // IE8 & IE9
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+    } else {
+      // CORS not supported.
+      xhr = null;
+    }
+    return xhr;
+  };
+
+  var url = 'http://159.148.101.219:8091/api/564382A4-81E8-4C2B-AE7D-80B88EC2FCB6/register';
+  var method = 'POST';
+
+  var xhr = createCORSRequest(method, url);
+
+  xhr.onload = function(response) {
+    console.log(this.response);
+  };
+
+  xhr.onerror = function() {
+
+    //////////////////////////////////////////
+    // Error code goes here.
+    //////////////////////////////////////////
+
+  };
+
+  xhr.withCredentials = true;
+  xhr.send(JSON.stringify(customerData));
+  console.log(JSON.stringify(customerData));
+
+  }
+
+  //////////////////////////////////////////
+  // Function for navigation thru steps & saving filled data.
+  //////////////////////////////////////////
 
   var nextStep = function() {
+    console.log( currentForm)
+  serializeForm();
+  console.log(allDataForm);
+
     if (currentForm < 3) {
     $("#form-" + currentForm).hide();
     currentForm++
     $("#form-" + currentForm).show();
+  } else if ( currentForm = 3) {
+    test();
+    sendToApi();
   }
-  }
+}
 
   var prevStep = function() {
     if (currentForm > 1) {
@@ -68,6 +137,5 @@ $('input')
 
   $(document).on('submit', nextStep);
   $(".izvelne").click(prevStep);
-
 
 });
