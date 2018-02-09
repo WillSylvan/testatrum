f32c5e248164a500309ac0119725f94544564d78
Pavils <pavilsdzi@gmail.com>
2018-02-09 10:55:01 +0200

    commit

--- js/credit_info.js
+++ js/credit_info.js
@@ -2,6 +2,8 @@ $(function(){
 	let loanType = localStorage.getItem("loanType")
     let loanPrincipal = localStorage.getItem("loanPrincipal")
     let loanTerms = localStorage.getItem("loanTerms")
+    commission = 0
+    registrationFee = 0
     console.log(loanType, loanPrincipal, loanTerms)
 
     $('.kredits-table .amount').text(loanPrincipal+" EUR")
@@ -10,4 +12,7 @@ $(function(){
 	var d = new Date(priorDate)
 	console.log(today,d)
 	$("#kredits-forma-profils .pay_till").text(d.getDate()+"/"+d.getMonth()+"/"+d.getFullYear())
+
+	let total = Math.pow((((loanPrincipal + commission + registrationFee) / loanPrincipal, 365 / loanTerms) - 1) * 100).toFixed(2)
+	console.log(total)
 	})
